34a50ff12ae151f487a3d8f3dc6eb00be69355ad
entzu0901 <lamigo079@gmail.com>
2022-01-06 16:57:08 +0800

    完成靜態組件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
-  <div>
-      <student/>
-      <hr>
-      <school/>
+  <div id="app">
+    <div class="todo-container">
+       <div class="todo-wrap">
+         <my-header></my-header>
+         <list/>
+         <my-footer></my-footer>
+       </div>
+    </div>
   </div>
 </template>
 
 <script>
-import School from './components/school.vue'
-import Student from './components/student.vue'
+import myFooter from './components/myFooter.vue'
+import myHeader from './components/myHeader.vue'
+import list from './components/list.vue'
+
 export default {
   name:'App',
   components:{
-    Student,
-    School
+    myFooter,
+    myHeader,
+    list
   },
 }
 </script>
 
 <style>
-
+   body{
+     background: #fff;
+   }
+   .btn{
+     display: inline-block;
+     padding:4px 12px;
+     margin-bottom: 0px;
+     font-size: 14px;
+     line-height: 20px;
+     text-align: center;
+     vertical-align: middle;
+     cursor: pointer;
+     box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),0 1px 2px rgba(0, 0, 245, 0.32);
+     border-radius: 4px;
+   }
+   .btn-danger{
+     color:#fff;
+     background-color: #da4f49;
+     border:1px solid #bd362f;
+   }
+   .btn-danger:hover{
+     color:#fff;
+     background-color: #bd362f;
+   }
+   .btn:focus{
+     outline: none;
+   }
+   .todo-container{
+     width: 600px;
+     margin:0 auto;
+   }
+   .todo-container .todo-wrap{
+     padding:10px;
+     border:1px solid #ddd;
+     border-radius: 5px;
+   }
 </style>
---
+++ src/components/list.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="todo-main">
+     <item></item>
+     <item></item>
+     <item></item>
+  </ul>
+</template>
+
+<script>
+import item from './item.vue'
+export default {
+    name:'list',
+    components:{
+        item
+    }
+}
+</script>
+
+<style scoped>
+    .todo-main{
+        margin-left: 0px;
+        border:1px solid #ddd;
+        border-radius: 2px;
+        padding: 0px;
+    }
+    .todo-empty{
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding-left: 5px;
+        margin-top: 10px;
+    }
+</style>
