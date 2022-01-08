2c178efdcb75d086de3f733e254bd5b903cfac55
entzu0901 <lamigo079@gmail.com>
2022-01-08 10:52:04 +0800

    todolist運用事件總線

---
+++ src - 全局事件總線$bus/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+// 引入app組件 他是所有組件的父組件
+import App from './App.vue'
+
+
+// 創建vue
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  beforeCreate() {
+    Vue.prototype.$bus= this  //安裝全局事件總線
+  },
+})
