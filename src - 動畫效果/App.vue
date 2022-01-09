1e4ca50a577e0a717e0fc0479ad34d2d0ab31b6e
entzu0901 <lamigo079@gmail.com>
2022-01-09 13:17:34 +0800

    todolist加動畫效果

---
+++ src - 動畫效果/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <test></test>
+  </div>
+</template>
+
+<script>
+import test from './components/test.vue'
+
+export default {
+  name:'app',
+  components:{
+    test
+},
+ 
+}
+</script>
+
+<style>
+  
+</style>
---
+++ src - 動畫效果/components/test.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <button @click="isShow=!isShow">顯示/隱藏</button>
+        <transition appear>
+            <h1 v-show="isShow">你好哦</h1>
+        </transition>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'test',
+    data() {
+        return {
+            isShow:true,
+        }
+    },
+}
+</script>
+
+<style scoped>
+    h1{
+        background-color: orange;
+    }
+    .v-enter-active{
+        animation: move 1s linear;
+    }
+    .v-leave-active{
+        animation: move 1s reverse;
+    }
+    @keyframes move {
+        from{
+            transform: translateX(-100%);
+        }
+        to{
+            transform: translateX(0px);
+        }
+    }
+</style>
