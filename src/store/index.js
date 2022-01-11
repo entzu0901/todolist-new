a28cf4d70256b41189d7747b5b76fbf32ae1843c
entzu0901 <lamigo079@gmail.com>
2022-01-11 21:37:55 +0800

    vuex中的各種map

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,14 @@ Vue.use(Vuex)
 // 該文件用於創建vuex中最為核心的store
 const actions={
     // 用於響應組件裡的動作
-    // jia(miniStore,value){
-    //     console.log('action中的jia被調用了')
-    //     miniStore.commit('JIA',value)
-    // },
-    // jian(miniStore,value){
-    //     console.log('action中的jian被調用了')
-    //     miniStore.commit('JIAN',value)
-    // }
+    jia(miniStore,value){
+        console.log('action中的jia被調用了')
+        miniStore.commit('JIA',value)
+    },
+    jian(miniStore,value){
+        console.log('action中的jian被調用了')
+        miniStore.commit('JIAN',value)
+    }
 
 }
 const mutations={
@@ -25,12 +25,21 @@ const mutations={
 }
 const state={
     // 用於存儲數據
-    sum:0
+    sum:9,
+    school:'blibli',
+    subject:'前端'
+}
+// 準備getters用於將state中的數據進行加工
+const getters={
+    bigSum(state){
+        return state.sum*10
+    }
 }
 // 創建store
 export default new Vuex.Store({
     actions,
     mutations,
-    state
+    state,
+    getters
 })
 
